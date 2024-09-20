8601f61531160dd12afd23da2fd409c67ae42dc2
ZavarkoZeno <zavarkoz@kkszki.hu>
2024-09-20 13:10:00 +0200

    Result is completed.

--- Program.cs
+++ Program.cs
@@ -15,6 +15,22 @@ namespace terulet
             int a;
             int b;
             int T;
+
+            do
+            {
+                Console.WriteLine("Kérem 'a' oldal értékét (0> kell lennie): ");
+                a = int.Parse(Console.ReadLine());
+            } while (a <= 0);
+
+            do
+            {
+                Console.WriteLine("Kérem 'b' oldal értékét (0> kell lennie): ");
+                b = int.Parse(Console.ReadLine());
+            } while (b <= 0);
+
+            T = a * b;
+
+            Console.WriteLine($"A téglalap területe: {T}");
         }
         static void Main(string[] args)
         {
